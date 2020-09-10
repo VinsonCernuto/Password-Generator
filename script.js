8116da1d80d7996c3eeaf2f651ad4850c28b6db5
Vinson Cernuto <cernutovinson@gmail.com>
2020-09-10 15:38:12 -0400

    added password variables for characters, numbers, and alphabet

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// User variables
+
 var enter;
+
 var confirmNumber;
+
 var confirmCharacter;
+
 var confirmUppercase;
-var confirmLowercase:
+
+var confirmLowercase;
+
+// Password variable
+
+// characters 
+
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+// Numbers
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Alphabet
+
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Write password to the #password input
 function writePassword() {
@@ -16,4 +34,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
+  ps = generatePassword();
+  document.getElementById("password").placeholer = ps;
+}
