193a94416b0012b0b2ed14eeb37e3a5ee0d80514
Vinson Cernuto <cernutovinson@gmail.com>
2020-09-10 15:44:12 -0400

    Added function to convert alphabet to uppercase

--- script.js
+++ script.js
@@ -24,6 +24,16 @@ number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+//function to convert to uppercase if user choises to
+
+var toUpper = function (x) {
+  return x.toUpperCase();
+}
+
+//making variable for convertion 
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
